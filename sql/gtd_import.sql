d1b91bcc4e6460325a8339322e00e45b3164748c
kademarcus <kmsp240@uky.edu>
2026-03-19 13:25:25 -0400

    Update gtd_import.sql
    adding tables to database (year, country, region, attack type, weapon, type)

--- sql/gtd_import.sql
+++ sql/gtd_import.sql
@@ -32,8 +32,83 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 -- Make sure all rows imported
-SELECT COUNT(*) FROM attacks;
+SELECT COUNT(*) FROM attacks; -- Around 200k expected
 
--- Storage and Tables
+-- Data Head
 SELECT * FROM attacks
+LIMIT 10;
+
+-- Attacks by year
+CREATE TABLE attacks_by_year AS
+	SELECT year, 
+		COUNT(*) AS total_attacks, 
+		SUM(success) AS successful_attacks,
+		SUM(suicide) AS suicide_attacks,
+		SUM(number_killed) AS total_killed,
+        SUM(casualties_clean) AS total_casualties
+    FROM attacks
+    GROUP BY year
+    ORDER BY year;
+    
+SELECT * FROM attacks_by_year
+LIMIT 10;
+
+-- Attacks by country
+CREATE TABLE attacks_by_country AS
+	SELECT country,
+		COUNT(*) AS total_attacks,
+		SUM(success) AS succesful_attacks,
+		SUM(suicide) AS suicide_attacks,
+        SUM(number_killed) AS total_killed,
+        SUM(casualties_clean) AS total_casualties
+	FROM attacks
+  GROUP BY country
+  ORDER BY total_attacks DESC;
+    
+SELECT * FROM attacks_by_country
+LIMIT 10;
+
+-- Attacks by region
+CREATE TABLE attacks_by_region AS
+	SELECT region,
+		COUNT(*) AS total_attacks,
+		SUM(success) AS succesful_attacks,
+		SUM(suicide) AS suicide_attacks,
+        SUM(number_killed) AS total_killed,
+        SUM(casualties_clean) AS total_casualties
+	FROM attacks 
+  GROUP BY region
+  ORDER BY total_attacks DESC;	
+
+SELECT * FROM attacks_by_region
+LIMIT 10;
+
+-- Attacks by type 
+CREATE TABLE attacks_by_type AS 
+	SELECT attack_type,
+		COUNT(*) AS total_attacks,
+		SUM(success) AS succesful_attacks,
+		SUM(suicide) AS suicide_attacks,
+        SUM(number_killed) AS total_killed,
+        SUM(casualties_clean) AS total_casualties
+	FROM attacks 
+  GROUP BY attack_type
+  ORDER BY total_attacks DESC;
+
+SELECT * FROM attacks_by_type
+LIMIT 10;
+
+-- Attacks by target
+CREATE TABLE attacks_by_target AS 
+	SELECT target_type,
+		COUNT(*) AS total_attacks,
+		SUM(success) AS succesful_attacks,
+		SUM(suicide) AS suicide_attacks,
+        SUM(number_killed) AS total_killed,
+        SUM(casualties_clean) AS total_casualties
+	FROM attacks
+  GROUP BY target_type
+  ORDER BY total_attacks DESC;
+
+SELECT * FROM attacks_by_target
 LIMIT 10;
